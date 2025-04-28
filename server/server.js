e1504c89f43eeb03322cec6137a2858e0876e4e7
TheismCeemec <kausik.saha@theism>
2025-04-28 09:55:03 +0530

    Create Server- tanmay

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
-console.log("This is Server");
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables
+
+
+const app = express();
+const PORT = process.env.PORT || 9000;
+
+app.get("/", (req, res) => {
+    res.status(200).send("<h1>Welcome To the Server of Theism-HealthCheckups</h1>");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is Running at http://localhost:${PORT}`);
+});
